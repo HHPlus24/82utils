89e7c909a0b82019ae468484848714970baa5c09
HC-kang <weston0713@gmail.com>
2023-11-23 17:01:17 +0900

    Feat: modify isValidLandline

--- src/validation/isValidLandline.spec.ts
+++ src/validation/isValidLandline.spec.ts
@@ -20,4 +20,24 @@ describe('isValidLandline', () => {
   it('should return true if the number is a landline(032, 11 character)', () => {
     expect(isValidLandline('032-1234-5678')).toBe(true);
   });
+
+  it('should return false if the number is not a landline(034, 11 character)', () => {
+    expect(isValidLandline('034-1234-5678')).toBe(false);
+  });
+
+  it('should return false if the number is not a landline(045, 10 character)', () => {
+    expect(isValidLandline('045-123-4567')).toBe(false);
+  });
+
+  it('should return false if the number is not a landline(056, 11 character)', () => {
+    expect(isValidLandline('056-1234-5678')).toBe(false);
+  });
+
+  it('should return false if the number is not a landline(065, 11 character)', () => {
+    expect(isValidLandline('065-1234-5678')).toBe(false);
+  });
+
+  it('should return false if the number is not a landline(070, 10 character)', () => {
+    expect(isValidLandline('070-123-4567')).toBe(false);
+  });
 });
