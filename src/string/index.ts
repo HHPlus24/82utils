ed788901df791c0c2c2e206d5d0d8476a2925aff
Giwan Kim <giwan.kim@gmail.com>
2023-11-23 21:20:25 +0900

    feat(string): add `capitalize` to string package

--- src/string/index.ts
+++ src/string/index.ts
@@ -1,2 +1,3 @@
 export * from './truncate';
 export * from './pad';
+export * from './capitalize';
--- src/string/pad.ts
+++ src/string/pad.ts
@@ -1,7 +1,7 @@
-export function padStart(str: string, length: number = 0, pad: string = ' ') {
+export function padStart(str: string, length: number = 0, pad: string = ' '): string {
     return str.padStart(length, pad);
 }
 
-export function padEnd(str: string, length: number = 0, pad: string = ' ') {
+export function padEnd(str: string, length: number = 0, pad: string = ' '): string {
     return str.padEnd(length, pad);
 }
