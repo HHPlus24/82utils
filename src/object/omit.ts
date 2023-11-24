e131c1e1805eaf23b54b63b8f7e611d4ad5b259c
Giwan Kim <giwan.kim@gmail.com>
2023-11-24 09:43:57 +0900

    feat(object): add `omit` to object package

---
+++ src/object/omit.ts
@@ -0,0 +1,5 @@
+export function omit<T extends object, K extends keyof T>(object: T, keys: K[]): Omit<T, K> {
+  const omitted = { ...object }
+  keys.forEach(key => delete omitted[key])
+  return omitted
+}
