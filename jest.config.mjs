c7ad62ca3949afda56b0073b5b44eeaed5ab07c2
HC-kang <weston0713@gmail.com>
2023-11-24 22:35:55 +0900

    Chore: modify jest and ts settings and add test:cov

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,12 +2,14 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     'ts-jest': {
       useESM: true,
     },
   },
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
 };
